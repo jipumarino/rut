93582f8bdc161d99b88b38ed00a8454f4ee7d443
Juan Ignacio Pumarino <jipumarino@gmail.com>
2009-09-01 22:15:41 -0400

    DV one liner un poco mejor

--- rut.rb
+++ rut.rb
@@ -17,7 +17,7 @@ end
 private
 
 def dv(rut)
-  ((0..9).to_a+["K","0"])[11-(rut.to_s.reverse.split("").map{|x| x.to_i}).zip((2..7).to_a * ((rut.size/6.0).ceil)).inject(0){|x,y| x+=y.first*y.last}%11].to_s
+  ((0..9).to_a+["K","0"])[11-(rut.to_s.reverse.split("").map{|x| x.to_i}).zip((0..rut.size).map{|x| x%6+2}).inject(0){|x,y| x+=y[0]*y[1]}%11].to_s
 end
 
 
